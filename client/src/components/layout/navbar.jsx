194e52d94d7fa0e66ed6718915a94704a826f076
ShreyaPopat0807 <shreya.popat120861@marwadiuniversity.ac.in>
2026-03-11 10:23:26 +0530

    navbar logout change
    navbar.jsx

--- client/src/components/layout/navbar.jsx
+++ client/src/components/layout/navbar.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { setTheme } from "../../utils/theme";
 import { useNavigate } from "react-router-dom";
 
-
 const SunIcon = () => (
     <svg className="w-5 h-5 text-primary dark:text-white" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
         <circle cx="12" cy="12" r="5" />
@@ -35,7 +34,6 @@ const ThemeSwitcher = () => {
             onClick={toggleTheme}
             className="relative w-14 h-8 flex items-center rounded-full bg-slate-200 dark:bg-slate-700 transition-all duration-500 p-1 cursor-pointer"
         >
-            {/* Sliding Circle */}
             <div
                 className={`absolute w-6 h-6 rounded-full bg-white shadow-md transform transition-all duration-500 flex items-center justify-center
                 ${theme === "dark" ? "translate-x-6 bg-primary" : "translate-x-0 bg-white"}`}
@@ -45,10 +43,26 @@ const ThemeSwitcher = () => {
         </button>
     );
 };
+
 const Navbar = () => {
     const navigate = useNavigate();
     const [hovered, setHovered] = useState(null);
     const [menuOpen, setMenuOpen] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken");
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem("accessToken");
+        setIsLoggedIn(false);
+        navigate("/");
+        window.location.reload();
+    };
 
     return (
         <header className="fixed top-0 z-50 w-full bg-white dark:bg-background-dark backdrop-blur-xl border-b border-slate-300 dark:border-slate-800/50">
@@ -71,6 +85,7 @@ const Navbar = () => {
                         </a>
                     </div>
                 </div>
+
                 <nav className="hidden min-[950px]:flex items-center gap-10">
                     {["Platform", "Solutions", "Resources", "Pricing"].map((item, index) => (
                         <a
@@ -88,47 +103,45 @@ const Navbar = () => {
                         </a>
                     ))}
                 </nav>
+
                 <div className="flex items-center gap-4">
-                    <a
-                        onClick={() => navigate("/login")}
-                        className=" cursor-pointer hidden min-[950px]:block px-5 py-2.5 text-slate-700 dark:text-slate-400 font-bold text-sm transition-all duration-300 hover:text-primary dark:hover:text-sage hover:scale-105"
-                    >
-                        Log In
-                    </a>
+
+                    {!isLoggedIn ? (
+                        <a
+                            onClick={() => navigate("/login")}
+                            className="cursor-pointer hidden min-[950px]:block px-5 py-2.5 text-slate-700 dark:text-slate-400 font-bold text-sm transition-all duration-300 hover:text-primary dark:hover:text-sage hover:scale-105"
+                        >
+                            Log In
+                        </a>
+                    ) : (
+                        <a
+                            onClick={handleLogout}
+                            className="cursor-pointer hidden min-[950px]:block px-5 py-2.5 text-red-500 font-bold text-sm transition-all duration-300 hover:scale-105"
+                        >
+                            Logout
+                        </a>
+                    )}
+
                     <button className="hidden min-[950px]:block shimmer-btn bg-primary hover:shadow-primary/30 text-white px-6 py-2.5 rounded-full font-bold text-sm shadow-xl transition-all duration-300 hover:scale-105 active:scale-95 cursor-pointer">
                         Start Free Trial
                     </button>
 
                     <ThemeSwitcher />
 
-                    {/* Hamburger */}
                     <button
                         onClick={() => setMenuOpen(!menuOpen)}
                         className="min-[950px]:hidden w-10 h-10 rounded-lg flex flex-col justify-center items-center gap-1 bg-primary dark:bg-primary transition-all duration-300 cursor-pointer"
                     >
-                        <span
-                            className={`w-6 h-[2px] bg-white transition-all duration-300 ${menuOpen ? "rotate-45 translate-y-[6px]" : ""
-                                }`}
-                        ></span>
-                        <span
-                            className={`w-6 h-[2px] bg-white transition-all duration-300 ${menuOpen ? "opacity-0" : ""
-                                }`}
-                        ></span>
-                        <span
-                            className={`w-6 h-[2px] bg-white transition-all duration-300 ${menuOpen ? "-rotate-45 -translate-y-[6px]" : ""
-                                }`}
-                        ></span>
+                        <span className={`w-6 h-[2px] bg-white transition-all duration-300 ${menuOpen ? "rotate-45 translate-y-[6px]" : ""}`}></span>
+                        <span className={`w-6 h-[2px] bg-white transition-all duration-300 ${menuOpen ? "opacity-0" : ""}`}></span>
+                        <span className={`w-6 h-[2px] bg-white transition-all duration-300 ${menuOpen ? "-rotate-45 -translate-y-[6px]" : ""}`}></span>
                     </button>
                 </div>
-
-
-
             </div>
 
             {/* Mobile Menu */}
-            <div
-                className={`fixed top-20 right-0 h-full w-72 shadow-2xl transform transition-transform duration-500 ease-in-out z-40 min-[950px]:hidden ${menuOpen ? "translate-x-0" : "translate-x-full"}`}>
-                <div className="p-6 flex flex-col gap-6  bg-white dark:bg-background-dark h-screen border-l border-slate-700">
+            <div className={`fixed top-20 right-0 h-full w-72 shadow-2xl transform transition-transform duration-500 ease-in-out z-40 min-[950px]:hidden ${menuOpen ? "translate-x-0" : "translate-x-full"}`}>
+                <div className="p-6 flex flex-col gap-6 bg-white dark:bg-background-dark h-screen border-l border-slate-700">
 
                     {["Platform", "Solutions", "Resources", "Pricing"].map((item, index) => (
                         <a
@@ -143,13 +156,27 @@ const Navbar = () => {
 
                     <div className="border-t border-slate-200 dark:border-slate-700 my-4"></div>
 
-                    <a
-                        href="/login"
-                        onClick={() => setMenuOpen(false)}
-                        className="px-5 py-2.5 text-slate-700 dark:text-slate-400 font-bold text-sm transition-all duration-300 hover:text-primary dark:hover:text-sage hover:scale-105"
-                    >
-                        Log In
-                    </a>
+                    {!isLoggedIn ? (
+                        <a
+                            onClick={() => {
+                                navigate("/login");
+                                setMenuOpen(false);
+                            }}
+                            className="px-5 py-2.5 text-slate-700 dark:text-slate-400 font-bold text-sm"
+                        >
+                            Log In
+                        </a>
+                    ) : (
+                        <a
+                            onClick={() => {
+                                handleLogout();
+                                setMenuOpen(false);
+                            }}
+                            className="px-5 py-2.5 text-red-500 font-bold text-sm"
+                        >
+                            Logout
+                        </a>
+                    )}
 
                     <button
                         onClick={() => setMenuOpen(false)}
@@ -161,11 +188,8 @@ const Navbar = () => {
                 </div>
             </div>
 
-            {/* Overlay */}
             {menuOpen && (
-                <div
-                    onClick={() => setMenuOpen(false)}
-                ></div>
+                <div onClick={() => setMenuOpen(false)}></div>
             )}
         </header>
     );
